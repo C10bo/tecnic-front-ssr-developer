e0fbba54729a5d180e214f14dc45df0157a62acd
c10bo <milogerrero@gmail.com>
2025-02-13 01:13:27 -0500

    create book form and alaing the form to center

--- biblioteca-frontend-ssr-developer/src/app/pages/books.module.ts
+++ biblioteca-frontend-ssr-developer/src/app/pages/books.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { BooksComponent } from './books/books.component';
 import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    BooksComponent
+    
   ],
   imports: [
     CommonModule,
